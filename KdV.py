cbdc6aa804739d9804c92050dea58199d2b50f1f
gloutch <ismael.herve@yahoo.com>
2017-04-09 15:09:49 +0200

    need test after correction of Newton_Raphson

--- KdV.py
+++ KdV.py
@@ -1,17 +1,62 @@
 import numpy as np
+# import scipy.optimize as spyo
+from newton import Newton_Raphson
 
-size = 10
 
-delta_t = 0.1
+delta_t = 1
 delta_x = 0.1
 epsilon = 0.1
 
+# global vector and matrix
+size = 3
+Un = np.empty(size)
+DG_matrix = np.empty([size, size])
+
+# constantes pour G
 delta_2x = delta_x * 2
 a = epsilon / (4 * delta_x)
 b = epsilon / (12 * (delta_x ** 3))
 
-U_n = np.empty(size)
+# constantes pour DG
+i_plus_2  = epsilon / (12 * (delta_x ** 3)) 
+i_diag    = epsilon / (4 * delta_x)
+i_minus_2 = - i_plus_2
+c = (1 / delta_2x) - (2 * i_plus_2)
 
+
+
+# init Un and GD_matrix
+def init_kdv(X0):
+        global Un, DG_matrix
+        
+        n = len(X0)
+        Un = np.copy(X0)
+        
+        DG_matrix = np.zeros([n, n])
+        # diagonal
+        for i in range(n):
+                DG_matrix[i][i] = i_diag
+        # diagonal +2 and -2
+        for i in range(n - 2):
+                DG_matrix[i][i + 2] = i_plus_2
+                DG_matrix[i + 2][i] = i_minus_2
+        DG_uptade(X0)
+
+# update DG_matrix,
+# juste need to change two diagonal thus O(n)
+def DG_uptade(U):
+        global GD_matrix
+        n = len(U)
+        # diagonal +1 and -1
+        for i in range(n - 1):
+                DG_matrix[i][i + 1] =  c + (i_diag * (U[i] + (2 * U[i + 1])))
+                DG_matrix[i + 1][i] = -c - (i_diag * (U[i] + (2 * U[i - 1])))
+
+def DG(U):
+        DG_uptade(U)
+        return DG_matrix
+
+# return the vector G(U)
 def G(U):
 	n = len(U)
 	V = np.empty(n)
@@ -29,11 +74,19 @@ def G(U):
 	V[i] = (U[0] - U[i-1]) / delta_2x
 	V[i] += a * (U[0] - U[i-1]) * (U[i-1] + U[i] + U[0])
 	V[i] += b * (U[1] - 2*U[0] + 2*U[i-1] - U[i-2])
-
 	return V
 
+def F(U):
+	return ((U - Un) / delta_t) + G((Un + U) / 2)
 
-def F(X):
-	return ((X - U_n) / delta_t) + G((U_n + X) / 2)
-	
-	
+def DF(U):
+        return ((1/delta_t) * np.identity(len(U)) + (DG((U - Un) / 2) / 2))
+
+def next_kdv(U):
+        global Un
+        Un = Newton_Raphson(F, DF, U, 8, 0.01)
+        return Un
+
+
+np.set_printoptions(suppress=True)
+
--- waves.py
+++ waves.py
@@ -2,35 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as an
 
+from KdV import init_kdv, next_kdv
+
+
 fig, ax = plt.subplots()
 L = 20.0
-N = 100
-alpha = 0.05
-
-wave, = plt.plot([], [], linewidth = 1.0, animated=True)
+N = 200
+alpha = 2.0
 
 xaxis = np.empty(N)
-theta = np.empty(N)
+yaxis = np.empty(N)
+
+
+def X0(alpha):
+    U = np.empty(N)
+    for i in range(N):
+        U[i] = np.exp(-alpha * np.square(xaxis[i]))
+    return U
+
 
 def init():
+    global ax, wave, xaxis, yaxis
+    # init the frame
     ax.set_xlim(-L, L)
     ax.set_ylim(0,1)
-    for i in range(0, N, 1):
-        xaxis[i] = ((2.0*i-N+1)/(N-1))*L
-        theta[i] = np.exp(-alpha*np.square(xaxis[i]))
-    wave.set_data(xaxis, theta)
+
+    # init coord
+    for i in range(N):
+        xaxis[i] = ((2.0 * i - N + 1)/(N-1)) * L
+    yaxis = X0(alpha)
+
+    init_kdv(yaxis)    
+    wave.set_data(xaxis, yaxis)
     return wave,    
-        
+
+
 def update(frame):
-    global alpha
-    alpha = alpha + 0.05
-    for i in range(0, N, 1):
-        theta[i] = np.exp(-alpha*np.square(xaxis[i]))
-    wave.set_data(xaxis, theta)
+    global yaxis
+    yaxis = next_kdv(yaxis)
+    # yaxis = X0(1)
+    wave.set_data(xaxis, yaxis)
     return wave,
 
-ani = an.FuncAnimation(fig, update, np.arange(1, 200),init_func=init, blit=True)
 
-plt.show()    
+wave, = plt.plot([], [], linewidth = 1.0, animated=True)
+ani = an.FuncAnimation(fig, update, np.arange(1, 200),init_func=init, blit=True)
+plt.show()
 
 

2d1a2dea5b3a09dca1a85d0a1b2f37d8abce75cf
LUCAS Hugo <hlucas002@mentawai.pedago.ipb.fr>
2017-04-09 16:04:34 +0200

    "résolution de quelques problèmes"

--- lagrangian.py
+++ lagrangian.py
@@ -1,20 +1,43 @@
-def elastic_force(u, u0, k):
-    return [[-k * (u[0] - u0[0])], [-k* (u[1] - u0[1])]]
-
-def J_elastic(u , u0, k):
-    return [[-k], [0]
-            [0], [-k]]
+from newton import *
 
 def centrifugal_force(u, u0, k):
-    res = [[0], [0]]
-    for i in range (2):
-        res[i][0] = -k * (u[i] - u0[i])/((u[0] - u0[0])**2 +(u[1] - u0[1])**2)**(3/2)
-    return res
+    return np.array([[-k * (u[0][0] - u0[0][0])], [-k* (u[1][0] - u0[1][0])]])
 
-def J_centrifugal_force(u , u0, k):
-    denominateur = ((u[0] - u0[0])**2 + (u[1] - u0[1])**2)**(5/2)
-    return [[k*(2((u[0] - u0[0])**2 - (u[1] - u0[1])**2))/denominateur], [3*k*(u[0] - u0[0])*(u[1] - u0[1])/denominateur]
-            [3*k*(u[0] - u0[0])*(u[1] - u0[1])/denominateur], [k*(2((u[1] - u0[1])**2 - (u[0] - u0[0])**2))/denominateur]]
+def J_centrifugal_force(u, u0, k):
+    return np.array([[-k, 0],
+                    [0, -k]])
 
 def gravitational_force(u, u0, k):
-    
+    res = np.array([[0], [0]])
+    for i in range (2):
+        res[i][0] = -k * (u[i][0] - u0[i][0])/((u[0][0] - u0[0][0])**2 +(u[1][0] - u0[1][0])**2)**(3/2)
+    return res
+
+def J_gravitational_force(u, u0, k):
+    denominateur = ((u[0][0] - u0[0][0])**2 + (u[1][0] - u0[1][0])**2)**(5/2)
+    return np.array([[k*(2*((u[0][0] - u0[0][0])**2 - (u[1][0] - u0[1][0])**2))/denominateur,
+                    3*k*(u[0][0] - u0[0][0])*(u[1][0] - u0[1][0])/denominateur],
+                    [3*k*(u[0][0] - u0[0][0])*(u[1][0] - u0[1][0])/denominateur,
+                    k*(2*((u[1][0] - u0[1][0])**2 - (u[0][0] - u0[0][0])**2))/denominateur]])
+
+
+def Newton_example(U):
+    u0 = np.array([[0], [0]])
+    k1 = 1
+    u01 = np.array([[0], [0]])
+    GF1 = gravitational_force(U, u01, k1)
+
+    k2 = 0.01
+    u02 = np.array([[1], [0]])
+    GF2 = gravitational_force(U, u02, k2)
+
+    k = 1
+    u03 = np.array([[0], [0]])
+
+    f = lambda U: gravitational_force(U, u01, k1) + gravitational_force(U, u02, k2) + centrifugal_force(U, u03, k)
+    J = lambda U: J_gravitational_force(U, u01, k1) + J_gravitational_force(U, u02, k2) + J_centrifugal_force(U, u03, k)
+
+    return Newton_Raphson_backtracking(f, J, U, 100, 0.01)
+
+U = np.array([[1.5], [0]])
+print(Newton_example(U))
--- newton.py
+++ newton.py
@@ -4,11 +4,12 @@ import pylab as pl
 def Newton_Raphson(f, J, U0, N, epsilon):
     i = 0
     while(i < N and np.linalg.norm(U0) > epsilon):
-        U0 = U0 + np.linalg.lstsq(J(U0), -f(U0))
+        U0 = U0 + np.linalg.lstsq(J(U0), -f(U0))[0]
+
         i += 1
     if (i == N):
-        display("Error : Solution not found")
-    return UO
+        print("Error : Solution not found")
+    return U0
 
 
 def Newton_Raphson_backtracking(f, J, U0, N, epsilon):
@@ -16,13 +17,13 @@ def Newton_Raphson_backtracking(f, J, U0, N, epsilon):
     while(i < N and np.linalg.norm(U0) > epsilon):
         last_U = U0
         last = np.linalg.norm(last_U)
-        U0 = U0 + np.linalg.lstsq(J(U0), -f(U0))
+        U0 = U0 + np.linalg.lstsq(J(U0), -f(U0))[0]
         while(np.linalg.norm(f(U0)) > last):
             U0 = (last_U + U0)/2
         i += 1
     if (i == N):
-        display("Error : Solution not found")
-    return UO
+        print("Error : Solution not found")
+    return U0
 
 def Newton_real(f, f1, x0, N, epsilon):
     i = 0

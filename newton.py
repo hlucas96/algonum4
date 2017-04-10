61d8d466cabe8ba857079531fda5953b861c8a0b
LUCAS Hugo <hlucas002@jbay.pedago.ipb.fr>
2017-04-10 12:01:22 +0200

    lagrangian and newton end

--- newton.py
+++ newton.py
@@ -5,10 +5,7 @@ def Newton_Raphson(f, J, U0, N, epsilon):
     i = 0
     while(i < N and np.linalg.norm(U0) > epsilon):
         U0 = U0 + np.linalg.lstsq(J(U0), -f(U0))[0]
-
         i += 1
-    if (i == N):
-        print("Error : Solution not found")
     return U0
 
 
@@ -21,8 +18,6 @@ def Newton_Raphson_backtracking(f, J, U0, N, epsilon):
         while(np.linalg.norm(f(U0)) > last):
             U0 = (last_U + U0)/2
         i += 1
-    if (i == N):
-        print("Error : Solution not found")
     return U0
 
 def Newton_real(f, f1, x0, N, epsilon):

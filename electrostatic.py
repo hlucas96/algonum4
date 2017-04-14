8d3bdfb5eed1e40eefa225fdf7bddb44b5602632
lucboissieras <lucboissieras@gmail.com>
2017-04-14 11:35:35 +0200

    electrostatic updated

--- electrostatic.py
+++ electrostatic.py
@@ -1,42 +1,54 @@
 from math import *
 import numpy as np
+import numpy.polynomial.legendre as npl
 import matplotlib.pyplot as plt 
 import newton as nr
 
 epsilon = 0.05
-iteration_number = 10
-
-def energy_electrostatic(E):
-    n = len(E)
-    x = 0
-    for i in range (0, n):
-        x += log(abs(E[i] + 1)) + log(abs(E[i] - 1))
-        s = 0
-        for j in range (0, i):
-            s += log(abs(E[i] - E[j]))
-        for j in range (i + 1, n):
-            s += log(abs(E[i] - E[j]))
-        x += (s/2)
-    return np.full(n,x)
+iteration_number = 100
 
 def jacobian_electrostatic(E):
     n = len(E)
-    J = np.empty(n)
+    J = np.empty((n,n))
     for i in range (0, n):
-        J[i] = (1/(E[i] + 1)) + (1/(E[i] - 1))
+        for j in range(0, i):
+           J[i][j] = -1/(pow((E[i] - E[j]),2))
+
+        J[i][i] = -1/(pow((E[i] + 1),2)) - 1/(pow((E[i] - 1),2))
+        for k in range (0, i):
+            J[i][i] -= 1/(pow((E[i] - E[k]),2))
+        for k in range (i + 1, n):
+            J[i][i] -= 1/(pow((E[i] - E[k]),2))
+
+        for j in range(i + 1, n):
+            J[i][j] =  -1/(pow((E[i] - E[j]),2))
+
+    return J
+
+def energy_electrostatic(E):
+    n = len(E)
+    F = np.empty(n)
+    for i in range (0, n):
+        F[i] = (1/(E[i] + 1)) + (1/(E[i] - 1))
         for j in range (0, i):
-            J[i] += 1/(E[i] - E[j])
+            F[i] += 1/(E[i] - E[j])
         for j in range (i + 1, n):
-            J[i] += 1/(E[i] - E[j])
-    J.shape = (n, 1)
-    return J   
+            F[i] += 1/(E[i] - E[j])
+        print(F[i])
+    return F
+
 
 def draw_electrostatic(E):
     X = nr.Newton_Raphson(energy_electrostatic, jacobian_electrostatic, E, iteration_number, epsilon)
-    charges = plt.plot(X, linestyle = 1.0)
-    plt.legend(charges, "charges positions")
+    charges = plt.scatter(X, np.full(len(X), 2))
+    #L = npl.legroots(npl.legder(E))
+    #legendre = plt.scatter(L, np.full(len(L), 2), color="r")
+
+    #    plt.legend(charges, "charges positions")
     plt.show()
 
 n = 9
 E = np.random.rand(n) * 2 - 1
-draw_electrostatic(E)
+energy_electrostatic(E)
+jacobian_electrostatic(E)
+#draw_electrostatic(E)
